47740311d1136fbcde505284b83b83a60b24ec52
Taz Campbell <shelbyacampbell2724@gmail.com>
2024-05-08 15:24:14 -0400

    started on the model

--- Codex/models.py
+++ Codex/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
-# Create your models here.
+class Codex(models.Model):
+
+    name=models.TextField("")
+
+    description=models.TextField("")
+
+    size=models.TextChoices("tiny", "small", "medium", "large", "huge", "gargantuan")
+
+    type=models.TextChoices("Aberration", "Beast", "Celestial", "Construct", "Dragon", "Elemental", "Fey", "Fiend", "Giant", "Humanoid", "Monstrosity", "Ooze", "Plant", "Swarm", "Undead")
+
+    alignement=models.TextChoices("Chaotic Evil", "Lawful Evil", "Evil", "Chaotic Good", "Lawful Good", "Good", "Neutral Evil", "Neutral Good", "True Neutral", "Unaligned")
+
+    armorClass=models.PositiveIntegerField()
+
+    hitPoints=models.PositiveIntegerField()
+
+    hitDice=models.TextField("")
+
+    
+
